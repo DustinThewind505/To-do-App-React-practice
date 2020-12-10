c36f9cf5a89293920df2777856f588b5808fec56
unknown <Dustin.Guillen14@yahoo.com>
2020-12-09 19:49:53 -0700

    to-do-functional, set up file structure

---
+++ to-do-functional/src/components/notes/NotesContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function NotesContainer() {
+
+
+
+
+    return(
+        <div className='note-list'>
+            <h2>Ello Poppet</h2>
+        </div>
+    )
+}
+
+export default NotesContainer;
